5a416791f0e177347c65c186c563e0182829b16a
LifthNik <hikki20088@gmail.com>
2022-10-20 03:02:09 +0300

    0.2

--- src/components/moviesList/movies.js
+++ src/components/moviesList/movies.js
@@ -1,8 +1,8 @@
+import {useSearchParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 
-import Movie from "../movieListCard/movie";
 import {movieService} from "../../services";
-import {useSearchParams} from "react-router-dom";
+import Movie from "../movieListCard/movie";
 
 
 
@@ -18,25 +18,27 @@ export default function Movies () {
 
     }, [query]);
 
-    console.log(query.get('page'))
 
-let nextPage = () => {
-        setQuery(value => ({page:+value.get('page')+1}))
-}
+    let nextPage = () => {
+        setQuery(value => ({page: +value.get('page') + 1}))
+    };
 
     let prevPage = () => {
-        setQuery(value => ({page:value.get('page')-1}))
-    }
+        setQuery(value => ({page: value.get('page') - 1}))
+    };
 
     return (<div className='Movies'>
 
             {
                 movies.results?.map(movie => <Movie key={movie.id} movie={movie}/>)
             }
+
             <button disabled={+query.get('page') < 2} onClick={prevPage}>prev</button>
 
             <button disabled={+query.get('page') > 499} onClick={nextPage}>Next</button>
+
         </div>
+
     );
 };
 
