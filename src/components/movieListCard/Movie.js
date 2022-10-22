8d4049032b788143e0764930197c6afacf526b93
LifthNik <hikki20088@gmail.com>
2022-10-22 06:40:33 +0300

    0.4

--- src/components/movieListCard/Movie.js
+++ src/components/movieListCard/Movie.js
@@ -1,10 +1,12 @@
 import PosterPreview from "../posterPreview/PosterPreview";
 
 import css from "./Movie.module.css"
+import ReactStars from "react-rating-stars-component/dist/react-stars";
 
 export default function Movie ({movie}) {
 
-    let {title,original_language, poster_path, release_date, genre_ids} = movie;
+    let {title,original_language, poster_path, release_date, genre_ids,
+        vote_average} = movie;
 
     return (<div className={css.Movie}>
 
