51343b12648aae6f908b7cf25a9885191a3c0156
LifthNik <hikki20088@gmail.com>
2022-10-24 05:15:13 +0300

    0.6

--- src/services/movie.service.js
+++ src/services/movie.service.js
@@ -5,6 +5,7 @@ let movieService = {
 
     getAllMovies: (page =1) => axiosInstance.get(`${urls.movies}?page=${page}`),
     getOneMovie: (id) => axiosInstance.get(`${urls.movie}/${id}`),
+    movieSearch: (movie) => axiosInstance.get(`${urls.search}${movie}`),
 
 };
 
