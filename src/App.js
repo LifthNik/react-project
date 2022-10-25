64adda5c903b1e45fecfc37d57fdab97cd8de613
LifthNik <hikki20088@gmail.com>
2022-10-25 15:31:01 +0300

    1.1 rework

--- src/App.js
+++ src/App.js
@@ -16,8 +16,6 @@ function App() {
 
                 <Route path={'/'} element={<MainLayout/>}/>
 
-                <Route path={'movies'} element={<MainLayout/>}/>
-
                 <Route path={'/movie/:id'} element={<SingleMovie/>}/>
 
             </Routes>
