2ea1b954d4c9155a993a2eb6b8f8317808b512fa
LifthNik <hikki20088@gmail.com>
2022-10-26 03:35:24 +0300

    1.2 add Skeleton

--- src/components/movieInfo/SingleMovie.js
+++ src/components/movieInfo/SingleMovie.js
@@ -10,7 +10,7 @@ import Header from "../header/Header";
 import css from './SingleMovie.css'
 import ReactStars from "react-rating-stars-component";
 import {Badge} from "reactstrap";
-import {Skeleton} from "@mui/material";
+import {Button,Skeleton} from "@mui/material";
 
 
 export default function SingleMovie() {
@@ -28,45 +28,51 @@ export default function SingleMovie() {
     let navigate = useNavigate();
 
 
-
-
-    //Skeleton
+    //Skeleton Loading
     let [loading, setLoading] = useState(true);
 
     useEffect(() => {
         setTimeout(() => {
-            setLoading(false)}, 2000)
+            setLoading(false)},2000)
     })
 
     return (<div className='SingleMovie'>
 
-            {loading ? (<Skeleton variant={"text"} height={"60px"}/>) : <Header/>}
-
-
-            <button className='backButton' onClick={() => navigate(-1)}>Back</button>
-
-            {loading ? (
-                    <Skeleton variant={"rectangular"} animation={'wave'} width={"100%"} height={'380px'}></Skeleton>
-                )
+            {loading ? (<Skeleton variant={"rounded"} height={"60px"}/>)
                 :
-                movie ? <div className='MovieInfo'>
+                <Header/>}
 
+            {loading ? (<Skeleton variant={"text"} height={"50px"} width={"80px"}/>) :
+                <Button variant={"outlined"} size={'small'} className='backButton'
+                        onClick={() => navigate(-1)}>Back</Button>}
+
+
+            {movie ? <div className='MovieInfo'>
+
+                    {loading ? (<Skeleton variant={"rounded"} height={'313px'} width={'512px'}/>)
+                        :
                         <div className='poster'>
-
                             <img src={`${posterURL + movie.backdrop_path}`} alt={movie.title}/>
-
                             <div className='genre'>
                                 {movie.genres.map(genre =>
                                     <Badge key={genre.id} color="primary" pill={true}>{genre.name} </Badge>)}
                             </div>
-
-                        </div>
-
-                        <div className='description'>
-                            <h2 className='title'>{movie.title}</h2>
-                            <div className='original_title'>{movie.original_title}</div>
+                        </div>}
 
 
+                    <div className='description'>
+
+                        {loading ? (<Skeleton variant={"text"} height={'39px'} width={'371px'}/>)
+                            :
+                            <h2 className='title'>{movie.title}</h2>}
+
+                        {loading ? (<Skeleton variant={"text"} height={'10px'} width={"150px"}/>)
+                            :
+                            <div className='original_title'>{movie.original_title}</div>}
+
+
+                        {loading ? (<Skeleton variant={"text"} height={'61px'} width={"213px"}/>)
+                            :
                             <div className='vote_count'>
                                 Total Votes: {movie.vote_count}
                                 <ReactStars
@@ -76,40 +82,54 @@ export default function SingleMovie() {
                                     edit={true}
                                     size={25}
                                 />
-                            </div>
+                            </div>}
 
+                        {loading ? (<Skeleton variant={"text"}/>)
+                            :
                             <div className='release_date'>
                                 Release: {movie.release_date}
-                            </div>
+                            </div>}
 
+                        {loading ? (<Skeleton variant={"text"}/>)
+                            :
                             <div className='popularity'>
                                 Popularity: {movie.popularity}
-                            </div>
+                            </div>}
 
+                        {loading ? (<Skeleton variant={"text"}/>)
+                            :
                             <div className='language'>
                                 Language: {movie.spoken_languages.map(lang =>
                                 <span key={lang.name}>{lang.english_name} </span>)}
-                            </div>
+                            </div>}
 
+                        {loading ? (<Skeleton variant={"text"}/>)
+                            :
+                            <div className='budget'>Budget: {movie.budget}$</div>}
 
-                            <div className='budget'>Budget: {movie.budget}$</div>
-
+                        {loading ? (<Skeleton variant={"text"}/>)
+                            :
                             <div className='country'>Country: {movie.production_countries.map(country =>
                                 <span key={country.iso_3166_1}>{country.name} </span>)}
-                            </div>
+                            </div>}
 
-                            <div className='webSite'>Web Site: <a href={movie.homepage}>{movie.homepage}</a></div>
+                        {loading ? (<Skeleton variant={"text"}/>)
+                            :
+                            <div className='webSite'>Web Site: <a href={movie.homepage}>{movie.homepage}</a></div>}
 
-                            <div className='tagLine'>Tagline: {movie.tagline}</div>
-
-                        </div>
-
-                        <div className='overview'>Description: {movie.overview}</div>
+                        {loading ? (<Skeleton variant={"text"}/>)
+                            :
+                            <div className='tagLine'>Tagline: {movie.tagline}</div>}
 
                     </div>
-                    :
-                    <h1>No movie</h1>
-            }
+
+                    {loading ? (<Skeleton variant={"text"} height={'150px'} width={'100%'}/>)
+                        :
+                        <div className='overview'>Description: {movie.overview}</div>}
+
+                </div>
+                :
+                <h1>No movie</h1>}
 
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
+import {Provider, useSelector} from "react-redux";
 import {setupStore} from "./redux";
 
 import App from './App';
@@ -18,9 +18,7 @@ root.render(
 
 <Provider store={store}>
 <BrowserRouter>
-
     <App/>
-
 </BrowserRouter>
 </Provider>
 
